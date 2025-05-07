caa0062f7dc17452e36f61a3f6c8cdda541624a6
Doselk <selker.dominik@gmail.com>
2025-05-07 13:50:27 +0200

    loose kopplung für Passwesen modul

---
+++ src/main/java/de/hsos/swa/gateway/PersonPassClient.java
@@ -0,0 +1,15 @@
+package de.hsos.swa.gateway;
+
+import de.hsos.swa.entity.PersonPass;
+
+import java.util.Optional;
+
+public interface PersonPassClient {
+
+    /**
+     * Fragt den Pass zum gegebenen Spieler (Person) ab.
+     * @param personId die ID des Spielers
+     * @return Optional mit PersonPass, falls vorhanden
+     */
+    Optional<PersonPass> fetchPass(String personId);
+}
