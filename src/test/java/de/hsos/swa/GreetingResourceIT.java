ee9ec3bbb98527f75178f2b9a13c91c7b50d75ff
quarkusio <no-reply@quarkus.io>
2025-05-07 09:59:35 +0000

    Initial commit

---
+++ src/test/java/de/hsos/swa/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package de.hsos.swa;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
